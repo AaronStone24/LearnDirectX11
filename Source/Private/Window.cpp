315ec5326a245a049967b59cb4d06c1d3bceefc8
Kartik Upadhyay <kartikup7@gmail.com>
2021-01-24 00:24:59 +0530

    Added custom icons

--- Source/Private/Window.cpp
+++ Source/Private/Window.cpp
@@ -1,5 +1,6 @@
 #include "../Public/Window.h"
 #include "../Public/WindowsMessageMap.h"
+#include "../Public/resource1.h"
 
 Window::WindowClass Window::WindowClass::wndClass;
 
@@ -14,12 +15,12 @@ Window::WindowClass::WindowClass() noexcept
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = GetInstance();
-	wc.hIcon = nullptr;
+	wc.hIcon = static_cast<HICON>(LoadImage(hInst, MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 32, 32, 0));;
 	wc.hCursor = nullptr;
 	wc.hbrBackground = nullptr;
 	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = GetName();
-	wc.hIconSm = nullptr;
+	wc.hIconSm = static_cast<HICON>(LoadImage(hInst, MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 16, 16, 0));;
 	RegisterClassEx(&wc);
 }
 
