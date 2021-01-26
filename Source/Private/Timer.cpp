23e05ace5127da6fc15f7ad6563774573b6497ec
Kartik Upadhyay <kartikup7@gmail.com>
2021-01-27 00:33:24 +0530

    Added the Timer class

---
+++ Source/Private/Timer.cpp
@@ -0,0 +1,21 @@
+#include "../Public/Timer.h"
+
+using namespace std::chrono;
+
+Timer::Timer()
+{
+	last = steady_clock::now();
+}
+
+float Timer::Mark()
+{
+	const auto old = last;
+	last = steady_clock::now();
+	const duration<float> frameTime = last - old;
+	return frameTime.count();
+}
+
+float Timer::Peek() const
+{
+	return duration<float>(steady_clock::now() - last).count();
+}
