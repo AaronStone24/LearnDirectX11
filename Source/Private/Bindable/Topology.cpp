8052ea4cacb02bc1fed4e81bd842a578f83bc2f7
Kartik Upadhyay <kartikup7@gmail.com>
2021-02-23 18:06:37 +0530

    Created Topology class

---
+++ Source/Private/Bindable/Topology.cpp
@@ -0,0 +1,11 @@
+#include "../../Public/Bindable/Topology.h"
+
+Topology::Topology(Graphics& gfx, D3D11_PRIMITIVE_TOPOLOGY type)
+	:
+	type(type)
+{}
+
+void Topology::Bind(Graphics& gfx) noexcept
+{
+	GetContext(gfx)->IASetPrimitiveTopology(type);
+}
