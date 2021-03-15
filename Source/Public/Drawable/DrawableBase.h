dd20c4ef4fc7cc95a7a03e7596e679a2bc3229fd
Kartik Upadhyay <kartikup7@gmail.com>
2021-03-15 09:26:49 +0530

    Works for any number of cubes

--- Source/Public/Drawable/DrawableBase.h
+++ Source/Public/Drawable/DrawableBase.h
@@ -17,9 +17,22 @@ public:
 	}
 	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept
 	{
-		assert(pIndexBuffer == nullptr);
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
 		pIndexBuffer = ibuf.get();
 		staticBinds.push_back(std::move(ibuf));
+	}
+	void SetIndexFromStatic() noexcept
+	{
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		for (const auto& b : staticBinds)
+		{
+			if (const auto p = dynamic_cast<IndexBuffer*>(b.get()))
+			{
+				pIndexBuffer = p;
+				return;
+			}
+		}
+		assert("Failed to find Index buffer in static binds" && pIndexBuffer != nullptr);
 	}
 private:
 	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept override
