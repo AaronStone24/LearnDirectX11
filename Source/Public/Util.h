c7859a56b6914bcab5d357b603854f545fa9d759
Kartik Upadhyay <kartikup7@gmail.com>
2021-03-17 10:38:41 +0530

    Added Util class

---
+++ Source/Public/Util.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+#define XMGLOBALCONST extern CONST __declspec(selectany)
+
+namespace Colors
+{
+	XMGLOBALCONST XMVECTORF32 White = { 1.0f, 1.0f, 1.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Black = { 0.0f, 0.0f, 0.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Red = { 1.0f, 0.0f, 0.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Green = { 0.0f, 1.0f, 0.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Blue = { 0.0f, 0.0f, 1.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Yellow = { 1.0f, 1.0f, 0.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Cyan = { 0.0f, 1.0f, 1.0f, 1.0f };
+	XMGLOBALCONST XMVECTORF32 Magenta = { 1.0f, 0.0f, 1.0f, 1.0f };
+}
